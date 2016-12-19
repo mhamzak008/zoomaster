15127881dc7b02014a8b4f73696089bf64eb10ad
Ege Berkay Gülcan <eldarfin@users.noreply.github.com>
2016-12-19 15:43:58 +0200

    Added functionalities to the species class

--- src/repository/Species.java
+++ src/repository/Species.java
@@ -1,50 +1,87 @@
 package repository;
 
+import java.awt.image.BufferedImage;
+
 public class Species {
 
+	private String name, latinName, countryOfOrigin, gender, feedingTime; // Due to MySQL constraints feeding time should be in HH:MM:SS format as a string
+	private int age, type, ID;
+	private BufferedImage image;
+	private static int id = 0;
+	
+	public Species(int type, int ID){
+		this.type = type;
+		if(id < 0)
+			this.ID = id++;
+		else
+			this.ID = ID;
+		name = "";
+		latinName = "";
+		countryOfOrigin = "";
+		gender = "";
+		feedingTime = "HH:MM:SS";
+		age = 0;
+		image = null;
+	}
+	
+	public Species(int type, int ID, int age, BufferedImage image, String...strings){
+		this.type = type;
+		this.age = age;
+		this.image = image;
+		name = strings[0];
+		feedingTime = strings[1];
+		latinName = strings[2];
+		countryOfOrigin = strings[3];
+		gender = strings[4];
+		if(id < 0)
+			this.ID = id++;
+		else
+			this.ID = ID;
+	}
+	
 	public String getName() {
 		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	public String getLatinName() {
 		// TODO Auto-generated method stub
-		return null;
+		return latinName;
 	}
 
 	public String getCountry() {
 		// TODO Auto-generated method stub
-		return null;
+		return countryOfOrigin;
 	}
 
 	public String getFeedingTime() {
 		// TODO Auto-generated method stub
-		return null;
+		return feedingTime;
 	}
 
-	public String getImage() {
+	public BufferedImage getImage() {
 		// TODO Auto-generated method stub
-		return null;
+		return image;
 	}
 
 	public int getAge() {
 		// TODO Auto-generated method stub
-		return 0;
+		return age;
 	}
 
 	public String getGender() {
 		// TODO Auto-generated method stub
-		return null;
+		return gender;
 	}
 
 	public int getID() {
 		// TODO Auto-generated method stub
-		return 0;
+		return ID;
 	}
 
 	public int getType() {
 		// TODO Auto-generated method stub
-		return 0;
+		return type;
 	}
 
 }
