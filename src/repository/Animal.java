a955fabbdfc7ac7e748040b9c9b2b78e098df716
Ege Berkay Gülcan <eldarfin@users.noreply.github.com>
2016-12-19 17:09:51 +0200

    Added functionalities for Animal and Plant classes

--- src/repository/Animal.java
+++ src/repository/Animal.java
@@ -1,10 +1,22 @@
 package repository;
 
 public class Animal extends Species{
+	
+	char doesHibernate;
+	
+	public Animal(int ID){
+		super(0, ID);
+		doesHibernate = 'N';
+	}
+	
+	public Animal(int ID, char doesHibernate){
+		super(0, ID);
+		this.doesHibernate = doesHibernate;
+	}
 
 	public char doesHibernate() {
 		// TODO Auto-generated method stub
-		return '0';
+		return doesHibernate;
 	}
 
 }
--- src/repository/Plant.java
+++ src/repository/Plant.java
@@ -1,15 +1,30 @@
 package repository;
 
 public class Plant extends Species{
+	
+	float lightNeed;
+	String lightTime; // Because of MySQL constraints it should be a string in format "HH:MM:SS"
 
+	public Plant(int ID){
+		super(1, ID);
+		lightNeed = 0;
+		lightTime = "HH:MM:SS";
+	}
+	
+	public Plant(int ID, float lightNeed, String lightTime){
+		super(1, ID);
+		this.lightNeed = lightNeed;
+		this.lightTime = lightTime;
+	}
+	
 	public float getLightNeed() {
 		// TODO Auto-generated method stub
-		return 0;
+		return lightNeed;
 	}
 
 	public String getLightTime() {
 		// TODO Auto-generated method stub
-		return null;
+		return lightTime;
 	}
 
 }
