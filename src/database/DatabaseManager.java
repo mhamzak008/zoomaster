1034a3439938ab20aca46bed35778e44b488ecab
Ege Berkay Gülcan <eldarfin@users.noreply.github.com>
2016-12-22 16:33:10 +0200

    Updated Class
    Updated tables, added hash indexes and search functionalities

--- src/database/DatabaseManager.java
+++ src/database/DatabaseManager.java
@@ -1,6 +1,9 @@
 package database;
 
+import java.awt.image.BufferedImage;
 import java.sql.*;
+import java.util.ArrayList;
+
 import repository.Animal;
 import repository.Plant;
 import repository.Species;
@@ -79,22 +82,23 @@ public class DatabaseManager {
 			
 			// Create species tables
 			String sql = "CREATE TABLE IF NOT EXISTS species(name VARCHAR(50),"
-					+ "latin_name VARCHAR(50), country_of_origin VARCHAR(50), "
+					+ "latin_name VARCHAR(50), country_of_origin VARCHAR(50), type INT,"
 					+ "feeding_time TIME, picture BLOB, age INT, gender VARCHAR(15),"
-					+ "sID int, PRIMARY KEY(sID)) ENGINE= InnoDB;";
+					+ "sID INT, PRIMARY KEY(sID), INDEX species_index(name) USING HASH) ENGINE= InnoDB;";
 			s.executeUpdate(sql);
 			
 			// Create animal table
-			sql = "CREATE TABLE IF NOT EXISTS animal(aID INT, hibernate CHAR(1),"
-					+ "sID INT, PRIMARY KEY(aID), FOREIGN KEY(sID) REFERENCES species(sID) ON DELETE CASCADE ON UPDATE CASCADE)"
+			sql = "CREATE TABLE IF NOT EXISTS animal(hibernate CHAR(1),"
+					+ "sID INT, PRIMARY KEY(sID), FOREIGN KEY(sID) REFERENCES species(sID) ON DELETE CASCADE ON UPDATE CASCADE, INDEX animal_index(sID) USING HASH)"
 					+ "ENGINE=InnoDB;";
 			s.executeUpdate("sql");
 			
 			//Create plant table
-			sql = "CREATE TABLE IF NOT EXISTS plant(pID INT, light_need FLOAT,"
-					+ "light_time TIME, sID INT, PRIMARY KEY(pID), FOREIGN KEY(sID) ON DELETE CASCADE ON UPDATE CASCADE"
-					+ "REFERENCES species(sID)) ENGINE=InnoDB;";
+			sql = "CREATE TABLE IF NOT EXISTS plant(light_need FLOAT,"
+					+ "light_time TIME, sID INT, PRIMARY KEY(sID), FOREIGN KEY(sID)"
+					+ "REFERENCES species(sID)  ON DELETE CASCADE ON UPDATE CASCADE, INDEX plant_index(sID) USING HASH) ENGINE=InnoDB;";
 			s.executeUpdate(sql);
+			
 		}catch(SQLException se){
 			return false;
 		}catch(Exception e){
@@ -117,7 +121,7 @@ public class DatabaseManager {
 		return true;
 	}
 	
-	public boolean addDataEntry(Species s, int index){
+	public boolean addDataEntry(Species s){
 		
 		Connection con = null;
 		Statement stmt = null;
@@ -139,7 +143,8 @@ public class DatabaseManager {
 			
 			// Insert into species
 			String sql = "INSERT INTO species VALUES("+ s.getName() + ", " + s.getLatinName() + ", " + s.getCountry() + ", " +
-					s.getFeedingTime() + ", " + s.getImage() + ", " + s.getAge() + ", " + s.getGender() + ", " + index + ");";
+					s.getType() + ", " + s.getFeedingTime() + ", " + s.getImage() + ", " + s.getAge() + ", " + s.getGender() + ", " 
+					+ s.getsID() + ");";
 			stmt.executeUpdate(sql);
 			
 			// If it is animal
@@ -147,14 +152,14 @@ public class DatabaseManager {
 				// Insert into animal
 				Animal a = (Animal) s;
 				
-				sql = "INSERT INTO species VALUES(" + a.getID() + ", " + a.doesHibernate() + ", " + index + ");";
+				sql = "INSERT INTO animal VALUES(" + a.doesHibernate() + "," + a.getsID() + ");";
 				stmt.executeUpdate(sql);
 			} else {
 				// Insert into animal
 				Plant p = (Plant) s;
 				
-				sql = "INSERT INTO species VALUES(" + p.getID() + ", " + p.getLightNeed() + ", " + p.getLightTime() + ", "
-						+ index + ");";
+				sql = "INSERT INTO plant VALUES(" + p.getLightNeed() + ", " + p.getLightTime() + ", "
+						+ p.getsID() + ");";
 				stmt.executeUpdate(sql);
 			}
 			
@@ -180,7 +185,7 @@ public class DatabaseManager {
 		return true;
 	}
 	
-	public boolean removeDataEntry(int index,int typeKey){
+	public boolean removeDataEntry(int id,int typeKey){
 		Connection con = null;
 		Statement stmt = null;
 		
@@ -202,16 +207,16 @@ public class DatabaseManager {
 			String sql;
 			if(typeKey == 0) {
 				// Delete from animal
-				sql = "DELETE FROM animal WHERE sID=" + index + ";";
+				sql = "DELETE FROM animal WHERE sID=" + id + ";";
 				stmt.executeUpdate(sql);
 			} else {
 				// Delete from plant
-				sql = "DELETE FROM plant WHERE sID=" + index + ";";
+				sql = "DELETE FROM plant WHERE sID=" + id + ";";
 				stmt.executeUpdate(sql);
 			}
 			
 			// Delete from species
-			sql = "DELETE FROM species WHERE sID=" + index + ";";
+			sql = "DELETE FROM species WHERE sID=" + id + ";";
 			stmt.executeUpdate(sql);
 			
 		}catch(SQLException se){
@@ -236,16 +241,16 @@ public class DatabaseManager {
 		return true;
 	}
 	
-	public boolean modifyDataEntry(int index, Species s){
+	public boolean modifyDataEntry(Species s){
 		//Since we do not know which value has been changed specifically,
 		//we first remove that row, then add the modified species as a new row.
 		
 		// Delete from database
-		boolean b = removeDataEntry(index, s.getType());
+		boolean b = removeDataEntry(s.getsID(), s.getType());
 		
 		if(b){
 			// Add new entry
-			b = addDataEntry(s, index);
+			b = addDataEntry(s);
 			
 			if(b) return true;
 		}
@@ -253,10 +258,10 @@ public class DatabaseManager {
 		return false;
 	}
 	
-	public Species getDataEntry(int index, int typeKey){
+	public ArrayList<Species> getDataEntry(String name){
 		Connection con = null;
 		Statement stmt = null;
-		Species s = null;
+		ArrayList<Species> result = new ArrayList<Species>();
 		
 		final String DB_DRIVER = "com.mysql.jdbc.Driver";
 		final String HOST_URL = "jdbc:mysql://localhost/zoomaster";
@@ -273,7 +278,42 @@ public class DatabaseManager {
 			// Create statement
 			stmt = con.createStatement();
 			
+			// Get species values
+			String sql = "SELECT * FROM species WHERE name LIKE '" + name + "%';	";
+			ResultSet rs = stmt.executeQuery(sql);
 					
+			// Add species to the result after getting animal or plant values
+			while(rs.next()){
+				int sID = rs.getInt("sID");
+				int age = rs.getInt("age");
+				String sName = rs.getString("name");
+				String lName = rs.getString("latin_name");
+				String country = rs.getString("country_of_origin");
+				String gender = rs.getString("gender");
+				String feedingTime = rs.getTime("feeding_time").toString();
+				BufferedImage image = (BufferedImage) rs.getBlob("picture");
+				int type = rs.getInt("type");
+				
+				if(type == 0){
+					sql = "SELECT * FROM animal WHERE sID=" + sID + ";";
+					ResultSet r = stmt.executeQuery(sql);
+					
+					char hibernate = r.getString("hibernate").charAt(0);
+					
+					Animal a = new Animal(sID, hibernate, age, image, sName, feedingTime, lName, country, gender);
+					result.add(a);
+				}else{
+					sql = "SELECT * FROM plant WHERE sID=" + sID + ";";
+					ResultSet r = stmt.executeQuery(sql);
+					
+					float lightNeed = r.getFloat("light_need");
+					String lightTime = r.getTime("light_time").toString(); 
+					Plant p = new Plant(sID, lightNeed, lightTime, age, image, sName, feedingTime, lName, country, gender);
+					result.add(p);
+				}
+				
+				
+			}
 		}catch(SQLException se){
 			return null;
 		}catch(Exception e){
@@ -293,7 +333,7 @@ public class DatabaseManager {
 		         e.printStackTrace();
 		    }			
 		}
-		return s;
+		return result;
 	}
 	
 	public static DatabaseManager getInstance(){
